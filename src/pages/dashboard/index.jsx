65042ba1de829d95f90cb8b09013ffe71673f342
Iska Mabo <maboiskanderr@gmail.com>
2024-02-29 16:34:44 +0600

    29.02.24 by iska

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
-import {Button, Card, Space} from "antd";
+import {Button, Card, Modal, Space} from "antd";
 import { BLOCKS } from "../../constants";
 import ActiveCategory from "../../components/ActiveCategory";
-import { Collapse } from 'antd';
+import { Collapse } from "antd";
 import "./style.scss";
 
 const DashBoard = () => {
   const [activeCategory, setActiveCategory] = useState(BLOCKS[0]);
   const [result, setResult] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState(null);
 
-  const handleCategoryClick = (item) => {
+    const showModal = (id) => {
+        setIsModalVisible(true);
+        setItemToDelete(id);
+    };
+
+    const handleOk = () => {
+        const filtered = result.filter((it) => it.id !== itemToDelete);
+        setResult(filtered);
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+
+    const handleCategoryClick = (item) => {
     setActiveCategory(item);
   };
 
@@ -17,12 +35,17 @@ const DashBoard = () => {
     setResult([...result, item]);
   };
 
-  const handleRemove = (e, id) => {
-      e.stopPropagation()
-      const filtered = result.filter(it => it.id !== id)
-      setResult(filtered)
-  }
+    const handleRemove = (e, id) => {
+        e.stopPropagation();
+        showModal(id);
+    };
 
+
+    // const handleRemove = (e, id) => {
+  //   e.stopPropagation();
+  //   const filtered = result.filter((it) => it.id !== id);
+  //   setResult(filtered);
+  // };
 
   const getTotal = () => {
     let total = 0;
@@ -31,81 +54,106 @@ const DashBoard = () => {
     return total;
   };
 
-    const items = result.map(item => {
-        const label = <div>
-            <div className="button_content mb-10">
-            <Button onClick={(e) => handleRemove(e, item.id)} danger>x</Button>
+  const items = result.map((item) => {
+    const label = (
+      <div className="flex justify-s alignC">
+        <div>
+          <h4 className="title">
+            {item.block.category_name} - {item.block.label}
+          </h4>
+          <div className="text">
+            Ширина: <b className="title">{item.width}</b>
+          </div>
+          <div className="text">
+            Итого: <b className="title">{item.total}</b>
+          </div>
+        </div>
+          <Button onClick={(e) => handleRemove(e, item.id)} danger>
+              x
+          </Button>
+
+          <Modal title="Подтверждение удаления" open={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+              <p>Вы уверены, что хотите удалить этот элемент?</p>
+          </Modal>
+      </div>
+    );
+    const text = (
+      <div className="flex flex-column gap-5">
+        {item.list.map((it) => {
+          return (
+            <div key={it.name} className="flex alignC gap-5">
+              <h5 className="title">{it.label}:</h5>
+              <div className="text">
+                {it.price} x {it.kef} = {it.subTotal.toFixed(1)}
+              </div>
             </div>
-            <h4 className="title">{item.block.category_name} - {item.block.label}</h4>
-            <div className="text">Ширина: <b className="title">{item.width}</b></div>
-            <div className="text">Итого: <b className="title">{item.total}</b></div>
+          );
+        })}
+        <hr />
+        <div className="text">
+          Цена за 1ед: <b className="title">{item.cost} c</b>
         </div>
-        const text = <div className="flex flex-column gap-5">
-            {item.list.map(it => {
-                return (
-                    <div key={it.name} className="flex alignC gap-5">
-                        <h5 className="title">{it.label}:</h5>
-                        <div className="text">{it.price} x {it.kef} = {it.subTotal.toFixed(1)}</div>
-                    </div>
-                )
-            })}
-            <hr/>
-            <div className="text">Цена за 1ед: <b className="title">{item.cost} c</b></div>
-            <div className="text">Количество: <b className="title">{item.count} шт</b></div>
-            <div className="text">Сумма: <b className="title">{item.total} c</b></div>
+        <div className="text">
+          Количество: <b className="title">{item.count} шт</b>
         </div>
-        return {
-            key: item.id,
-            label: label,
-            children: text,
-        }
-    })
+        <div className="text">
+          Сумма: <b className="title">{item.total} c</b>
+        </div>
+      </div>
+    );
+    return {
+      key: item.id,
+      label: label,
+      children: text,
+    };
+  });
 
   return (
     <div className="dash_board container_landing body_background">
-      <div className="content">
+      <div className="war">
+        <div className="content">
           {BLOCKS.map((item, i) => {
-              let cardClass = '';
-              switch (item.category_name) {
-                  case 'Нижние Блоки':
-                      cardClass = 'category-navy-blue';
-                      break;
-                  case 'Верхние Блоки':
-                      cardClass = 'category-coral';
-                      break;
-                  case 'Антресоль':
-                      cardClass = 'category-mint';
-                      break;
-                  case 'Пенал':
-                      cardClass = 'category-lavender';
-                      break;
-                  default:
-                      cardClass = '';
-              }
-              return (
-                  <Space key={i} wrap style={{ padding: 8 }}>
-                      <Card
-                          className={cardClass}
-                          title={item.label}
-                          onClick={() => handleCategoryClick(item)}
-                          style={{ width: 300, cursor: "pointer" }}
-                      >
-                          <p>{item.category_name}</p>
-                      </Card>
-                  </Space>
-              );
+            let cardClass = "";
+            switch (item.category_name) {
+              case "Нижние Блоки":
+                cardClass = "category-navy-blue";
+                break;
+              case "Верхние Блоки":
+                cardClass = "category-coral";
+                break;
+              case "Антресоль":
+                cardClass = "category-mint";
+                break;
+              case "Пенал":
+                cardClass = "category-lavender";
+                break;
+              default:
+                cardClass = "";
+            }
+            return (
+              <Space key={i} wrap style={{ padding: 8 }}>
+                <Card
+                  className={cardClass}
+                  title={item.label}
+                  onClick={() => handleCategoryClick(item)}
+                  style={{ width: 300, cursor: "pointer" }}
+                >
+                  <p>{item.category_name}</p>
+                </Card>
+              </Space>
+            );
           })}
+        </div>
+        <div>
+          {activeCategory && (
+            <ActiveCategory active={activeCategory} handleAdd={handleAdd} />
+          )}
+        </div>
       </div>
-      <div>
-        {activeCategory && (
-          <ActiveCategory
-            active={activeCategory}
-            handleAdd={handleAdd}
-          />
-        )}
-      </div>
-      <div>
-        <h2>Смета - <span style={{color: 'red'}}>{getTotal()} с</span></h2>
+      <div className="flower">
+        <h2>
+          Смета - <span style={{ color: "red" }}>{getTotal()} с</span>
+        </h2>
         <div>
           <Collapse items={items} bordered={false} />
           <hr />
